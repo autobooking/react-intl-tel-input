aa4c45ee72600d9b4800cb5a6428fbde29a82d85
kruglyjmax <kruglyj.max@gmail.com>
2021-12-28 04:46:09 +0200

    Add dist

---
+++ dist/index.d.ts
@@ -0,0 +1,13 @@
+import IntlTelInput, {
+  IntlTelInputProps,
+  IntlTelInputState,
+} from './components/IntlTelInput'
+import FlagDropDown, {
+  FlagDropDownProps,
+  FlagDropDownState
+} from './components/FlagDropDown'
+
+export default IntlTelInput
+export { IntlTelInputProps, IntlTelInputState }
+export { FlagDropDown, FlagDropDownProps, FlagDropDownState }
+export { CountryData } from './types'
---
+++ dist/index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "FlagDropDown", {
+  enumerable: true,
+  get: function get() {
+    return _FlagDropDown.default;
+  }
+});
+exports.default = void 0;
+
+var _IntlTelInput = _interopRequireDefault(require("./components/IntlTelInput"));
+
+var _FlagDropDown = _interopRequireDefault(require("./components/FlagDropDown"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _default = _IntlTelInput.default;
+exports.default = _default;
